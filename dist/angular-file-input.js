f8519426268232a190659a65d4a0207ea91810ab
Dale Myszewski II <dale.myszewski@seagate.com>
2018-06-18 19:43:17 -0500

    Added demo removed travis.yml

--- dist/angular-file-input.js
+++ dist/angular-file-input.js
@@ -10,12 +10,14 @@
         function link(scope, element, attrs, ngModel) {
             if (ngModel && element[0].tagName === 'INPUT' && fileTypeRegex.test(attrs['type'])) {
                 element.on('change', function () {
+                    console.log("Changed");
                     var input = this;
                     if ('multiple' in attrs) {
                         var files = Array.prototype.map.call(input.files, function (file) { return file; });
                         ngModel.$setViewValue(files);
                     }
                     else {
+                        console.log(input.files[0]);
                         ngModel.$setViewValue(input.files[0]);
                     }
                 });
